c3fadb4ae9868e19ecf08a887e0ee35309066c51
atenasadat <atenasaghi@yahoo.com>
2021-12-31 23:24:13 +0330

    Switch case refactoring

--- src/main/java/codegenerator/Address.java
+++ src/main/java/codegenerator/Address.java
@@ -3,7 +3,12 @@ package codegenerator;
 /**
  * Created by mohammad hosein on 6/28/2015.
  */
-public class Address {
+
+interface function {
+    public String toString();
+}
+
+public class Address implements function{
     public int num;
     public TypeAddress Type;
     public VarType varType;
@@ -19,16 +24,50 @@ public class Address {
         this.Type = TypeAddress.Direct;
         this.varType = varType;
     }
-    public String toString(){
-        switch (Type){
-            case Direct:
-                return num+"";
-            case Indirect:
-                return "@"+num;
-            case Imidiate:
-                return "#"+num;
-        }
-        return num+"";
+
+//    public String toString(){
+//       return this.toString();
+//
+//    }
+}
+
+
+class Direct extends Address implements function{
+
+
+    public Direct(int num, VarType varType) {
+        super(num, varType);
+    }
+    @Override
+    public String toString() {
+        return this.num+"";
     }
 }
 
+
+class Indirect extends Address implements function{
+
+
+    public Indirect(int num, VarType varType) {
+        super(num, varType);
+    }
+
+    @Override
+    public String toString() {
+        return "@"+ this.num;
+    }
+}
+
+
+class Imidiate extends Address implements function{
+
+
+    public Imidiate(int num, VarType varType) {
+        super(num, varType);
+    }
+
+    @Override
+    public String toString() {
+        return "#"+  this.num;
+    }
+}
