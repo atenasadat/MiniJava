9c78c5177e6bab2a8497d49f34ba6a850f091ec2
atenasadat <atenasaghi@yahoo.com>
2021-12-31 22:26:22 +0330

    class name convections

--- src/main/java/codegenerator/CodeGenerator.java
+++ src/main/java/codegenerator/CodeGenerator.java
@@ -289,14 +289,10 @@ public class CodeGenerator {
 
             Address s1 = ss.pop();
             Address s2 = ss.pop();
-//        try {
             if (s1.varType != s2.varType) {
                 ErrorHandler.printError("The type of operands in assign is different ");
             }
-//        }catch (NullPointerException d)
-//        {
-//            d.printStackTrace();
-//        }
+
             memory.add3AddressCode(Operation.ASSIGN, s1, s2, null);
 
     }
