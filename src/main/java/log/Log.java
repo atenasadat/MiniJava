f46a210f573c176206ab87091688f40998696104
fatemeh-bahrani <51292227+nfbahrani@users.noreply.github.com>
2022-01-01 22:38:44 +0330

    make Log.java a utility class (singleton)

--- src/main/java/log/Log.java
+++ src/main/java/log/Log.java
@@ -4,7 +4,12 @@ package log;
  * Created by mohammad hosein on 6/29/2015.
  */
 public class Log {
-    public static void print(String s)
+    private Log log;
+    public static Log createLog(){
+        this.log = new Log();
+        return this.log;
+    };
+    public void print(String s)
     {
 //        System.out.println(s);
     }
